a652abc14e5123eb06d8b1c005414ee29c1aac7c
JoyOrji <ninaorji5@gmail.com>
2025-09-21 16:02:37 +0100

    make changes

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+ let C = 'C is fun';
+ let pt = 'Python is cool';
+ let Ja = 'JavaScript is amazing';
+ const names = [C, Pt, Ja];
+ for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
